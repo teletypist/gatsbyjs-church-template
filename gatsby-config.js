2143dfaf59fee246e77589d7c061336b78d8132a
Frederick Rainbird <frainbird@gmail.com>
2018-12-19 11:23:08 +1100

    Changed store categories and products to json data types in src/store

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    plugins: [`gatsby-plugin-netlify-cms`],
+    plugins: [
+        `gatsby-transformer-json`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `./src/store/`,
+            },
+        },
+        `gatsby-plugin-netlify-cms`
+    ],
 }
