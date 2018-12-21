88375c6c27c272bf65014aa2bf675a940be423b6
Frederick Rainbird <frainbird@gmail.com>
2018-12-21 11:53:24 +1100

    Generating pages from data

---
+++ gatsby-node.js
@@ -0,0 +1,50 @@
+const path = require(`path`)
+const { createFilePath } = require('gatsby-source-filesystem')
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `CategoriesJson`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allCategoriesJson {
+          edges {
+            node {
+                fields {
+                  slug
+                }
+                id
+                title
+            }
+          }
+        }
+      }
+    `
+).then(result => {
+      result.data.allCategoriesJson.edges.forEach(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve(`./src/templates/category.js`),
+          context: {
+            // Data passed to context is available
+            // in page queries as GraphQL variables.
+            title: node.title,
+          },
+        })
+      })
+      resolve()
+    })
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,28 @@
 import React from "react"
+import {Link} from 'gatsby'
 
-export default () => <div>Hello world!</div>
+export default ({ data }) => <div>
+<ul>
+    {data.allCategoriesJson.edges.map(({node}) => 
+        <li key={node.id}>
+            <Link to={node.fields.slug}>{node.title}</Link>
+        </li>
+     )}
+</ul>
+</div>
+
+export const query = graphql`
+  query {
+    allCategoriesJson {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          title
+        }
+      }
+    }
+  }
+`
